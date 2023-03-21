40ccfe7e991c18f27ea2a8a7856611b5d0dad70d
Amartya (Marty) Mukherjee <am4521ty@gmail.com>
2023-03-21 14:29:16 -0400

    Created skeleton code for numerical solution

--- environments/PackCooling.py
+++ environments/PackCooling.py
@@ -4,7 +4,7 @@ from gym import spaces
 
 from environments.PackCoolingGraph import PackCoolingGraph
 
-LOOKBACK_WINDOW_SIZE = 0
+LOOKBACK_WINDOW_SIZE = 2047
 
 class PackCooling(gym.Env):
     """Custom Environment that follows gym interface"""
@@ -30,6 +30,7 @@ class PackCooling(gym.Env):
             self.h = self.h_linear
         else:
             # h(u) = exp(0.1*u)
+            self.F = None
             self.h = self.h_nonlinear
 
         # Initial condition to set for U. Set to cosine series to respect boundary conditions.
@@ -53,8 +54,30 @@ class PackCooling(gym.Env):
         return self.F @ u
 
     def step(self, action):
-        # Execute one time step within the environment
-        ...
+        # Execute N_iter time steps within the environment
+        for _ in range(self.N_iter):
+            state = np.concatenate((self.u,self.w))
+
+            if self.F:
+                # h(u) is a linear function: h(u) = Fu. (u,w) after one time step can be computed by inverting a matrix.
+                ... # Solve with matrix inversion
+            else:
+                # h(u) is a non-linear function: h(u) = exp(0.1u). (u,w) after one time step can be computed using Newton-Raphson method.
+                ... # Solve with Newton-Raphson method
+            
+            self.u = state[:self.u.shape[0]]
+            self.w = state[self.u.shape[0]:]
+            reward = -np.dot(self.u,self.u)
+
+            self.sigma_render[self.timestep] = action
+
+            self.timestep += 1
+
+            self.u_render[self.timestep,:] = self.u
+            self.w_render[self.timestep,:] = self.w
+
+        return state, reward, (self.timestep == LOOKBACK_WINDOW_SIZE), None
+
     def reset(self):
         # Reset the state of the environment to an initial state
 
@@ -64,14 +87,14 @@ class PackCooling(gym.Env):
         # Set w to a list of zeros
         self.w = np.zeros_like(self.x)
 
-        self.u_render = np.zeros((LOOKBACK_WINDOW_SIZE,self.x.shape[0]))
-        self.w_render = np.zeros((LOOKBACK_WINDOW_SIZE,self.x.shape[0]))
+        self.u_render = np.zeros((LOOKBACK_WINDOW_SIZE+1,self.x.shape[0]))
+        self.w_render = np.zeros((LOOKBACK_WINDOW_SIZE+1,self.x.shape[0]))
         self.sigma_render = np.zeros((LOOKBACK_WINDOW_SIZE))
 
         self.u_render[0,:] = self.u
         self.w_render[0,:] = self.w
 
-        self.t = 0
+        self.timestep = 0
         
     def render(self, mode='live', **kwargs):
         # Render the environment to the screen
@@ -80,6 +103,6 @@ class PackCooling(gym.Env):
                 self.visualization = PackCoolingGraph(
                     kwargs.get('title', None))
 
-            if self.t >= LOOKBACK_WINDOW_SIZE:
+            if self.timestep >= LOOKBACK_WINDOW_SIZE:
                 self.visualization.render(
                     self.u_render, self.w_render, self.sigma_render)
